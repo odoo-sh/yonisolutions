ac13525c4c1e7e9b281f19c8a6cdce3add8ce0f4
Sivaprakash <sivaprakash@sodexis.com>
2022-09-01 17:20:06 +0530

    [ID:25367]Update Website And Remove the other social media icons/links

---
+++ yonisolutions/README.rst
@@ -0,0 +1,22 @@
+=================
+Yoni Solutions
+=================
+
+Customizations for the Yoni Solutions Customer.
+
+Changes
+=======
+
+#. Created new group named "Can see Settings". Enabling this will show the Settings App.
+
+Usage
+=====
+
+To use this module, you need to:
+
+#. Install the app
+
+Contributors
+------------
+
+* Sodexis
---
+++ yonisolutions/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Sodexis
+# License OPL-1 (See LICENSE file for full copyright and licensing details).
+
+{
+    "name": "Yoni Solutions",
+    "summary": """
+        Customizations for the Yoni Solutions Customer.
+        """,
+    "version": "15.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "http://sodexis.com/",
+    "author": "Sodexis",
+    "license": "OPL-1",
+    "installable": True,
+    "application": False,
+    "depends": [
+            'website',
+    ],
+    "data": [
+        'views/website_templates.xml',
+    ],
+}
