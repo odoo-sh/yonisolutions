53b2830c65a5a4d30ff02a38fadefad1ad06f91d
Xavier Dass <xavier@sodexis.com>
2022-09-09 11:03:15 +0530

    [ID: 25354 ] Release to Production - Yoni

---
+++ customer_reports/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2020-22 Sodexis
+# License OEEL-1 (See LICENSE file for full copyright and licensing details).
+
---
+++ yonisolutions/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2020-22 Sodexis
+# License OEEL-1 (See LICENSE file for full copyright and licensing details).
